285a9d1fdbd1a144a376ff32a13b9630a0ecdcf6
Adam Smith <adaminhonolulu@gmail.com>
2024-09-07 14:50:08 -1000

    removes base router

--- app/routers/base_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-base_router = APIRouter()
-
-
-@base_router.get("/")
-async def root():
-    return {"message": "Welcome to the Star Wars API"}
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from app.routers.base_router import base_router
 from app.routers.characters_router import characters_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.include_router(base_router)
 app.include_router(characters_router)
