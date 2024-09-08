e46384da2487ad938a41d63f430cb58116d66908
Adam Smith <adaminhonolulu@gmail.com>
2024-09-07 14:52:04 -1000

    adds char not found test to router

--- tests/router_tests/test_characthers_router.py
+++ tests/router_tests/test_characthers_router.py
@@ -1,4 +1,5 @@
 from unittest.mock import patch
+from fastapi import HTTPException
 
 
 @patch("app.routers.characters_router.add_new_character")
@@ -10,6 +11,7 @@ def test_create_character_valid_data(
         "name": "Darth Vader",
     }
 
+    # Mock the successful return value of add_new_character
     mock_add_new_character.return_value = mock_star_wars_character_read
 
     # When
@@ -25,10 +27,30 @@ def test_create_character_valid_data(
     }
 
 
+@patch("app.routers.characters_router.add_new_character")
+def test_create_character_character_not_found(mock_add_new_character, client):
+    # Given
+    character_input_data = {
+        "name": "Unknown Character",
+    }
+
+    # Mock add_new_character to raise a ValueError when the character is not found
+    mock_add_new_character.side_effect = ValueError("Character not found in SWAPI")
+
+    # When
+    response = client.post("/characters/", json=character_input_data)
+
+    # Then
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Error while fetching character from SWAPI: Character not found in SWAPI"
+    }
+
+
 def test_create_character_invalid_data(client):
     # Given
     invalid_character_data = {
-        "name": 2,
+        "name": 2,  # Invalid type for 'name', should be a string
     }
 
     # When
